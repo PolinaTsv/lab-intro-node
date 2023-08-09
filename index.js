326af7ac741c058d13a5d4801351b0e3ee2e6d2a
PolinaTsv <polina.tsvetkova7@gmail.com>
2023-08-10 01:11:15 +0800

    Done

--- index.js
+++ index.js
@@ -1,17 +1,63 @@
 class SortedList {
-  constructor() {}
+  constructor(items) {
+    this.items = items.sort(compareNumbers);
+    this.length = this.items.length;
+  }
+  add(item) {
+    this.items.push(item);
+    this.items.sort(compareNumbers);
+  }
 
-  add(item) {}
+  get(pos) {
+    if (pos < 0 || this.length <= pos) {
+      throw new Error("OutOfBounds");
+    } else {
+      return this.items[pos];
+    }
+  }
 
-  get(pos) {}
+  max() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      console.log(Math.max(...this.items));
+    }
+  }
 
-  max() {}
+  min() {
+    if (this.items.length <= 0) {
+      throw new Error("EmptySortedList");
+    } else {
+      console.log(Math.min(...this.items));
+    }
+  }
 
-  min() {}
+  sum() {
+    const sum = sumNumbers(this.items);
+    console.log(sum);
+  }
 
-  sum() {}
-
-  avg() {}
+  avg() {
+    const avr = sumNumbers(this.items)/this.items.length;
+    console.log(avr);
+  }
 }
+
+function compareNumbers(a, b) {
+  return a - b;
+}
+function sumNumbers(arr){
+  const sum = arr.reduce(function (acc, val) {
+    return acc + val;
+  }, 0);
+  return sum;
+}
+
+const newList = new SortedList([2, 1, 9, 7]);
+newList.add(3);
+newList.max();
+newList.min();
+newList.sum();
+newList.avg();
 
 module.exports = SortedList;
